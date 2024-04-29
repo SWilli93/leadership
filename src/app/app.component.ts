cdf1ca16c4f6002ca093a77e63b579e3087d12cb
Scott <70402467+SWilli93@users.noreply.github.com>
2024-04-29 16:09:58 -0400

    dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PeopleService } from '../shared/people.service';
+import { JsonPipe } from '@angular/common'; 
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [JsonPipe],
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'leadership';
+  peopleArray: any;
+
+  constructor(private peopleService: PeopleService) { }
+
+  getPeople(){
+    this.peopleService.getPeople().subscribe(people => {
+        this.peopleArray = people
+    });
+  }
+
+  ngOnInit() {
+    this.getPeople();
+  }
 }
